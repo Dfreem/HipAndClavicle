e32f6cb9c4ef79b16fbcfcfd77f22b1cc3060c2f
Devin <freemand@my.lanecc.edu>
2023-04-11 15:08:35 -0700

    --wip-- [skip ci]

--- HipAndClavicle/Models/Listing.cs
+++ HipAndClavicle/Models/Listing.cs
@@ -6,14 +6,6 @@ namespace HipAndClavicle.Models
         public int ListingId { get; set; }
         public List<Image> Images { get; set; } = new();
         public double Price { get; set; } = default!;
-<<<<<<< HEAD
-        /// <summary>
-        /// Use this list of Colors as a list of colors.
-=======
-        /// <summary>
-        /// Use this list of Colors as a list of colors.
->>>>>>> 1f86abf4896a501a5e9520aa6c388dbe8d9ec0f5
-        /// </summary>
         public List<Color> Colors { get; set; } = new();
         public Product ListingProduct { get; set; } = default!;
         [Range(0, int.MaxValue)]
@@ -23,9 +15,5 @@ namespace HipAndClavicle.Models
         public int OnHand { get; set; } = default!;
         public string? shape { get; set; }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1f86abf4896a501a5e9520aa6c388dbe8d9ec0f5
     }
 }
