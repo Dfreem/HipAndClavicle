7582263a60fccbc58fffee89eb96f6314efaf58f
Devin Freeman <freemand@my.lanecc.edu>
2023-04-18 14:51:50 -0700

    updating docs and repos

--- HipAndClavicle/Usings.cs
+++ HipAndClavicle/Usings.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
@@ -38,3 +39,40 @@ global using shippingapi;
 global using shippingapi.Client;
 global using shippingapi.Model;
 global using shippingapi.Api;
+=======
+﻿global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Diagnostics;
+global using System;
+
+global using HipAndClavicle;
+global using HipAndClavicle.Repositories;
+global using HipAndClavicle.ViewModels;
+global using HipAndClavicle.Controllers;
+global using HipAndClavicle.Models;
+global using HipAndClavicle.Models.Enums;
+global using HipAndClavicle.Data;
+
+global using AspNetCoreHero;
+global using AspNetCoreHero.ToastNotification.Extensions;
+global using AspNetCoreHero.ToastNotification.Abstractions;
+global using AspNetCoreHero.ToastNotification;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Html;
+global using Microsoft.AspNetCore.WebUtilities;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http.Features;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Identity.UI;
+global using Microsoft.Extensions.Logging;
+global using System.Runtime.InteropServices;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using Pomelo.EntityFrameworkCore;
+>>>>>>> Stashed changes
