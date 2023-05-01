d7ba1f70c5c19d816ee8c7397e4db7614b7c4531
Nehemiah John <90234750+NAMSTERR@users.noreply.github.com>
2023-05-01 12:52:28 -0700

    Sprint 2 is done.

--- HipAndClavicle/Controllers/ContactController.cs
+++ HipAndClavicle/Controllers/ContactController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HipAndClavicle.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -81,14 +83,37 @@ namespace HipAndClavicle.Controllers
 
         public async Task<IActionResult> CustomerChat()
         {
-            var messages = _context.UserMessages.Select(m => new MessageViewModel
-            {
-                Id = m.Id,
-                Sender = m.SenderUserName,
-                Receiver = m.ReceiverUserName,
-                Content = m.Content,
-                DateSent = m.DateSent
-            }).ToList();
+            var customers = await _userManager.GetUsersInRoleAsync("Customer");
+            //var messages = _context.UserMessages
+            //    .Where(x => x.SenderUserName == User.Identity.Name || x.ReceiverUserName == User.Identity.Name)
+
+            //    .Select(m => new MessageViewModel
+            //{
+            //    Id = m.Id,
+            //    Sender = m.SenderUserName,
+            //    Receiver = m.ReceiverUserName,
+            //    Content = m.Content,
+            //    DateSent = m.DateSent
+            //}).ToList();
+
+            ViewBag.customers = customers.Where(c => c.UserName != User.Identity.Name).ToList();
+            var messages = new List<MessageViewModel>();
+            return View(messages);
+        }
+
+        public async Task<IActionResult> AllCustomerMesseges()
+        {
+            IList<AppUser> allAdmins = await _userManager.GetUsersInRoleAsync("Admin");
+
+            var messages = _context.UserMessages
+                .Select(m => new MessageViewModel
+                {
+                    Id = m.Id,
+                    Sender = m.SenderUserName,
+                    Receiver = m.ReceiverUserName,
+                    Content = m.Content,
+                    DateSent = m.DateSent
+                }).ToList();
             return View(messages);
         }
         [HttpPost]
@@ -98,29 +123,68 @@ namespace HipAndClavicle.Controllers
 
             // Get the other user
             var adminUsers = await _userManager.GetUsersInRoleAsync("Admin");
-
+            UserMessage userMessage = new UserMessage
+            {
+                Email = currentUser.Email,
+                Number = currentUser.PhoneNumber,
+                Content = customerMessage.Message,
+                SenderUserName = currentUser.UserName,
+            };
             if (customerMessage.SendTo == null)
             {
                 var admin = adminUsers.Count > 0 ? adminUsers[0] : null;
-
-                UserMessage userMessage = new UserMessage
-                {
-                    Email = currentUser.Email,
-                    Number = currentUser.PhoneNumber,
-                    Content = customerMessage.Message,
-                    SenderUserName = currentUser.UserName,
-                    ReceiverUserName = admin?.UserName
-                };
-
-                _context.UserMessages.Add(userMessage);
-                await _context.SaveChangesAsync();
+                userMessage.ReceiverUserName = admin?.UserName;
             }
+            else
+            {
+                userMessage.ReceiverUserName = customerMessage.SendTo;
+            }
+            _context.UserMessages.Add(userMessage);
+            await _context.SaveChangesAsync();
+
 
 
 
             return Json(new { success = true });
         }
 
+        [HttpGet]
+        public async Task<IActionResult> FilterMesseges(string customerName, string dateSent)
+        {
+            if (customerName == null || dateSent == null)
+            {
+                return BadRequest("customer name and date sent can not be empty");
+            }
+            var messageDate = (DateTime)DateTime.Parse(dateSent);
+            //
+            var messegesUser = _context.UserMessages
+                .Where(m => m.SenderUserName == customerName || m.ReceiverUserName == customerName
+               )
+                .AsEnumerable();
+            //.ToList();
+            var filtredByDate = messegesUser.Where(m => m.DateSent.Date == messageDate.Date).ToList();
+
+            return Ok(filtredByDate);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MesseagesWithCustomer(string username)
+        {
+            var currentUser = User.Identity.Name;
+
+            //var messegesUser = _context.UserMessages
+            //    .Where(m => ((m.SenderUserName == username && m.ReceiverUserName == currentUser) 
+            //    || (m.SenderUserName == currentUser && m.ReceiverUserName == username)
+            //    )
+            //   ).OrderBy(m => m.DateSent.Date).AsEnumerable();
+
+            var messegesUser = await _context.UserMessages
+    .Where(m => (m.SenderUserName == currentUser && m.ReceiverUserName == username) ||
+                (m.SenderUserName == username && m.ReceiverUserName == currentUser))
+    .ToListAsync();
+
+            return Ok(messegesUser);
+        }
     }
 
     public class CustomerMessage
