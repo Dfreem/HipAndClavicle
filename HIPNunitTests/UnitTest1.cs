b31f0c8679aaed2cee9dd5337e08033f77b70c76
Steven Brady <bradys@my.lanecc.edu>
2023-05-17 04:15:30 -0700

    added HIPNunitTests Project to the solution

---
+++ HIPNunitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HIPNunitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
