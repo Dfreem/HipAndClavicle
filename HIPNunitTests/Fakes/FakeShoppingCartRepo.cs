5382803d32922f1ab2216731dd9d9330a54687e9
Steven Brady <bradys@my.lanecc.edu>
2023-05-25 17:59:22 -0700

    added tests for add and edit

---
+++ HIPNunitTests/Fakes/FakeShoppingCartRepo.cs
@@ -0,0 +1,66 @@
+﻿using HipAndClavicle.Models;
+using HipAndClavicle.Repositories;
+using HipAndClavicle.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIPNunitTests.Fakes
+{
+    public class FakeShoppingCartRepo : IShoppingCartRepo
+    {
+        private List<ShoppingCartItem> _shoppingCartItems;
+
+        public FakeShoppingCartRepo()
+        {
+            _shoppingCartItems = new List<ShoppingCartItem>();
+        }
+
+        public Task<ShoppingCart> GetOrCreateShoppingCartAsync(string cartId, string ownerId)
+        {
+            // Implement the fake logic for GetOrCreateShoppingCartAsync if needed
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ShoppingCartItemViewModel>> GetShoppingCartItemsAsync(IEnumerable<ShoppingCartItem> items)
+        {
+            var viewModels = items
+                .Select(item => new ShoppingCartItemViewModel(item))
+                .ToList();
+
+            return Task.FromResult(viewModels);
+        }
+
+        public Task<ShoppingCartItem> GetCartItem(int id)
+        {
+            var item = _shoppingCartItems.FirstOrDefault(i => i.ShoppingCartItemId == id);
+            return Task.FromResult(item);
+        }
+
+        public async Task AddShoppingCartItemAsync(ShoppingCartItem item)
+        {
+            _shoppingCartItems.Add(item);
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateItemAsync(ShoppingCartItem item)
+        {
+            // Implement the fake logic for UpdateItemAsync if needed
+            await Task.CompletedTask;
+        }
+
+        public async Task RemoveItemAsync(ShoppingCartItem item)
+        {
+            _shoppingCartItems.Remove(item);
+            await Task.CompletedTask;
+        }
+
+        public async Task ClearShoppingCartAsync(string cartId, string ownerId)
+        {
+            _shoppingCartItems.Clear();
+            await Task.CompletedTask;
+        }
+    }
+}
--- HIPNunitTests/ShoppingCartTests.cs
+++ HIPNunitTests/ShoppingCartTests.cs
@@ -1,16 +1,106 @@
-﻿
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using HipAndClavicle.Controllers;
+using HipAndClavicle.Repositories;
+using HipAndClavicle.ViewModels;
+using HIPNunitTests.Fakes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
+
 namespace HIPNunitTests
 {
+    [TestFixture]
     public class ShoppingCartTests
     {
+        private ShoppingCartController _controller;
+        private FakeShoppingCartRepo _fakeShoppingCartRepo;
+        private FakeCustRepo _fakeCustRepo;
+        private Mock<IHttpContextAccessor> _mockHttpContextAccessor;
+
         [SetUp]
         public void Setup()
         {
+            // Create an instance of the fake repository
+            _fakeShoppingCartRepo = new FakeShoppingCartRepo();
+            _fakeCustRepo = new FakeCustRepo();
+
+            // Create a mock for IHttpContextAccessor and set up the HttpContext
+            _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new DefaultHttpContext();
+            httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, "userId"),
+            new Claim(ClaimTypes.Name, "username"),
+            // Add other claims if needed
+            }));
+            _mockHttpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+
+            // Create an instance of the controller and pass the dependencies
+            _controller = new ShoppingCartController(_fakeShoppingCartRepo, _fakeCustRepo, _mockHttpContextAccessor.Object);
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
         }
+
         [Test]
-        public void Test1()
+        public async Task Index_ReturnsViewWithViewModel()
         {
-            Assert.Pass();
+            // Act
+            var result = await _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ShoppingCartViewModel>(result.Model);
         }
+
+        [Test]
+        public async Task AddToCart_WithValidListingId_AddsItemToCart()
+        {
+            // Arrange
+            int listingId = 1;
+            int quantity = 2;
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "userId"),
+                new Claim(ClaimTypes.Name, "username"),
+                // Add other claims if needed
+            }, "mock"))
+                }
+            };
+
+            // Act
+            var result = await _controller.AddToCart(listingId, quantity) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual("ShoppingCart", result.ControllerName);
+        }
+
+        [Test]
+        public async Task UpdateCart_WithValidItemId_UpdatesCartItem()
+        {
+            // Arrange
+            int itemId = 1;
+            int quantity = 3;
+
+            // Act
+            var result = await _controller.UpdateCart(itemId, quantity) as RedirectToActionResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+
+        }
+
+        // Write similar tests for other methods in the ShoppingCartController
     }
 }
